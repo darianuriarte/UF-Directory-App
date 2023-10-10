cfbea08fc0502acfe31d7d4768171b141a60b868
darianuriarte <darianuriarte99@gmail.com>
2023-10-10 13:46:53 -0400

    Defined Schema in

--- ListingModel.js
+++ ListingModel.js
@@ -14,6 +14,7 @@ import { Sequelize, Model, DataTypes } from '@sequelize/core';
   Read - artilce to learn more about environment variables - https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
 */
 //ADD CODE HERE to connect to you database
+const sequelize = new Sequelize(process.env.API_URL);
 
 /* Create your Sequalize Model for Listing */
 /*Hint: Take a look at listings.json to figure out the model attributes we need to define.
@@ -24,6 +25,27 @@ import { Sequelize, Model, DataTypes } from '@sequelize/core';
 const Listing = sequelize.define('Listing', {
   // Model attributes are defined here
   //ADD CODE HERE
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  latitude: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  longitude: {
+    type: DataTypes.FLOAT,
+    allowNull: true
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+  
 }, {
   // Other model options go here
   tableName: 'Listings'
